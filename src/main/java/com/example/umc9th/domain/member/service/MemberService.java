257a3c0047eab7dbf8c45c34d9d5aca431672b21
박성진 <106302098+macboy5@users.noreply.github.com>
2025-11-10 02:07:15 +0900

    feat : mission 7

--- src/main/java/com/example/umc9th/domain/member/service/MemberService.java
+++ src/main/java/com/example/umc9th/domain/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.umc9th.domain.member.service;
 import com.example.umc9th.domain.member.dto.MyPageResponseDTO;
 import com.example.umc9th.domain.member.entity.Member;
 import com.example.umc9th.domain.member.repository.MemberRepository;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
